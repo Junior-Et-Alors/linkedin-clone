cc7c46a0c1a0b0f99540c42fd05ec4d9d8e920c1
nicoHikaru <nicolas.quei83@gmail.com>
2022-07-10 17:08:41 +0200

    :construction: feat(main): Add gin
    Added to be able to have the auto rebuild with auto reload

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ type Page struct {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello word")
+	fmt.Fprintf(w, "hello")
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -19,29 +19,14 @@ func Login(w http.ResponseWriter, r *http.Request) {
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "page coucou")
+	fmt.Fprintf(w, "page")
 }
-
-// func loadPage(title string) (*Page, error) {
-// 	filename := title + ".html"
-// 	body, err := os.ReadFile(filename)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &Page{Title: title, Body: body}, nil
-// }
-
-// func viewHandler(w http.ResponseWriter, r *http.Request) {
-// 	title := r.URL.Path[len("/view/"):]
-// 	p, _ := loadPage(title)
-// 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-// }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/login", Login)
 	http.HandleFunc("/ins", Register)
-	//http.HandleFunc("/view/", viewHandler)
+
 	http.ListenAndServe(":8080", nil)
 
 }
