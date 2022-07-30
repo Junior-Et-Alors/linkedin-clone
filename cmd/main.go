5bd8417f2d9b8a92ba08c72ea02f098e8fa563b1
nicoHikaru <nicolas.quei83@gmail.com>
2022-07-30 17:51:24 +0200

    :hammer: feat : add variable env
    add variable env

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"linkedin-clone/config/info"
 	infoLogger "linkedin-clone/logger"
 	"linkedin-clone/src/controller"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	port := ":8080"
+
+	port := os.Getenv("PORT")
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", controller.HomePage)
 	r.HandleFunc("/login", controller.LoginPage).Methods("POST")
-
 	r.HandleFunc("/register", controller.RegisterUser).Methods("POST")
 
 	// profil := r.PathPrefix("/profil").Subrouter()
 	// profil.HandleFunc("/parametre", controller.SettingPage)
 	// profil.HandleFunc("/info/{name:[a-z]+}", controller.ProfilPage)
 
-	go info.Server(r, port)
+	//go info.Server(r, port)
 	infoLogger.Error(r)
 
 	http.Handle("/", r)
